af11532c09d60bcdc822a44502bec9a58f377b99
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2017-05-12 13:16:45 +0430

    Add project files.

---
+++ LEARNING_CONCEPTS/Person.cs
@@ -0,0 +1,124 @@
+﻿namespace LEARNING_CONCEPTS
+{
+	//public class Person
+	//{
+	//	public int Age;
+	//	public string FullName;
+
+	//	public void ShowInfo()
+	//	{
+	//		System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+	//	}
+	//}
+
+	//public class Person
+	//{
+	//	/// <summary>
+	//	/// Default Constructor
+	//	/// Snippet => ctor
+	//	/// </summary>
+	//	public Person()
+	//	{
+	//	}
+
+	//	public int Age;
+	//	public string FullName;
+
+	//	public void ShowInfo()
+	//	{
+	//		System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+	//	}
+	//}
+
+	//public class Person
+	//{
+	//	public Person()
+	//	{
+	//	}
+
+	//	/// <summary>
+	//	/// این کار حرفه‌ای نیست
+	//	/// </summary>
+	//	//public int Age;
+	//	public int Age = 20;
+
+	//	/// <summary>
+	//	/// این کار حرفه‌ای نیست
+	//	/// </summary>
+	//	public string FullName = "";
+
+	//	public void ShowInfo()
+	//	{
+	//		System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+	//	}
+	//}
+
+	public class Person
+	{
+		public Person()
+		{
+			Age = 20;
+
+			//FullName = "";
+			FullName = string.Empty;
+		}
+
+		public int Age;
+		public string FullName;
+
+		public void ShowInfo()
+		{
+			System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+		}
+	}
+
+	//public class Person
+	//{
+	//	public Person(string fullName)
+	//	{
+	//		FullName = fullName;
+	//	}
+
+	//	public int Age;
+	//	public string FullName;
+
+	//	public void ShowInfo()
+	//	{
+	//		System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+	//	}
+	//}
+
+	/// <summary>
+	/// نگاه ذیل دیگر عرف نیست
+	/// </summary>
+	//public class Person
+	//{
+	//	public Person()
+	//	{
+	//	}
+
+	//	public Person(int age)
+	//	{
+	//		Age = age;
+	//	}
+
+	//	public Person(string fullName)
+	//	{
+	//		FullName = fullName;
+	//	}
+
+	//	public Person(string fullName, int age)
+	//	{
+	//		Age = age;
+	//		FullName = fullName;
+	//	}
+
+	//	public int Age;
+	//	public string FullName;
+
+	//	public void ShowInfo()
+	//	{
+	//		System.Console.WriteLine("I'm {0} and {1} years old.", FullName, Age);
+	//	}
+	//}
+}
---
+++ LEARNING_CONCEPTS/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace LEARNING_CONCEPTS
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Person oPerson = new Person();
+
+			//oPerson.Age = 20;
+			//oPerson.FullName = "Ali Reza Alavi";
+
+			//oPerson.ShowInfo();
+
+			//Circle oCircle = new Circle();
+
+			//oCircle.BorderWidth = 2;
+
+			//Circle oCircle = new Circle(2, 3, 10);
+
+			//Circle oCircle = new Circle(x: 2, y: 3, radius: 10); // OK
+
+			//Circle oCircle = new Circle(2, y: 3, radius: 10); // OK
+
+			//Circle oCircle = new Circle(2, y: 3, 10); // Error
+
+			//Circle oCircle = new Circle(radius: 10, x: 2, y: 3); // OK
+
+			//Circle oCircle = new Circle(x: 2, y: 3, radius: 10);
+
+			//Person oPerson = new Person() { Age = 20, FullName = "Ali Reza Alavi" };
+
+			//oPerson.ShowInfo();
+
+			System.Console.Write("Press [ENTER] To Exit...");
+			System.Console.ReadLine();
+		}
+	}
+}
